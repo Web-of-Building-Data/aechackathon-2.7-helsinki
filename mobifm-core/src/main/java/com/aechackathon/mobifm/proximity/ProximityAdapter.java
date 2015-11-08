def7a7146a90e8f1dabf0dfa76167f420dc20059
mansewiz <berthier_lemieux@hotmail.com>
2015-11-08 07:05:46 +0200

    Thingsee support

--- mobifm-core/src/main/java/com/aechackathon/mobifm/proximity/ProximityAdapter.java
+++ mobifm-core/src/main/java/com/aechackathon/mobifm/proximity/ProximityAdapter.java
@@ -2,6 +2,7 @@ package com.aechackathon.mobifm.proximity;
 
 import java.io.IOException;
 import java.net.URI;
+import java.util.Iterator;
 import java.util.Map;
 import java.util.stream.Collectors;
 
@@ -12,6 +13,10 @@ import javax.ws.rs.client.WebTarget;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 import javax.ws.rs.core.UriBuilder;
+
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.JSONValue;
 
 import com.aechackathon.mobifm.Main;
 import com.fasterxml.jackson.core.JsonParseException;
@@ -85,6 +90,68 @@ public class ProximityAdapter {
 		ObjectMapper mapper = new ObjectMapper();
 		Map<String, Object> jsonObject = mapper.readValue(jsonString, new TypeReference<Map<String, Object>>(){});
 		return jsonObject;
+
+	}
+
+	private void getThingseeData(String path) throws JsonParseException, JsonMappingException, IOException {
+
+
+		Client client = ClientBuilder.newClient();	
+		WebTarget target2 = ( client
+				.target(Main.getConfigProperty("thingsee.host"))
+				.path(Main.getConfigProperty("thingsee.path")));
+				
+		
+		Builder builder2 = target2.request();
+		builder2.accept(MediaType.APPLICATION_JSON);
+		builder2.header("Authorization", "Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ0cyI6MTQ0NjkxNDIwODM2NywidXVpZCI6ImE4NzliZGQwLTg1NjUtMTFlNS05YjRiLTg5NzczZmEwMzIxNCIsInNjb3BlIjpbImFsbDphbGwiXSwiaWF0IjoxNDQ2OTE0MjA4LCJleHAiOjE0NDc1MTkwMDh9.fewleb-PJZULfigRRDfJ_LFgNM8PKcOhdHaO-aSJLck");
+		String jsonString2 = builder2.get(String.class);
+		
+
+		Object obj=JSONValue.parse(jsonString2);
+		JSONObject jsonObject2 = (JSONObject) obj;
+
+		JSONArray events = (JSONArray) jsonObject2.get("events");
+		System.out.println(events);
+
+		JSONObject lastEvent = (JSONObject) events.get(0);
+		JSONObject cause = (JSONObject) lastEvent.get("cause");
+		
+		JSONArray senses = (JSONArray) cause.get("senses");
+		
+		
+		Double temperature = 0.0;
+		Double humidity = 0.0;
+		Double pressure = 0.0;
+		
+		Iterator<JSONObject> iterator = senses.iterator();
+		while (iterator.hasNext()) {
+			JSONObject reading = iterator.next();
+			String id = (String) reading.get("sId");
+			Double val = 0.0;
+			try {
+				val = (Double) reading.get("val");
+			}			
+			catch (Exception e)
+			{
+			}
+		
+			switch (id){
+            case "0x00060100":
+                humidity = val;
+                break;
+            case "0x00060200":
+                temperature = val;
+                break;
+            case "0x00060400":
+                pressure = val;
+                break;
+                
+            }
+		}
+
+		System.out.println("Temperature: " + temperature + " Humidity: " + humidity + " Pressure: " + pressure);
+
 
 	}
 	
